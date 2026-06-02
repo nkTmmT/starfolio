57b7d612d98986f63045361fe47fb9a82daa3919
TmmT <1090718046@qq.com>
2026-06-02 17:47:48 +0800

    update code

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,9 +3,9 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://alexmercer.dev",
+    url: "https://tmmt.dev",
     locale: "en_US",
-    twitterHandle: "@alexmercer_dev",
+    twitterHandle: "@tmmt_dev",
   },
 
   // ---------------------------------------------------------------------------
